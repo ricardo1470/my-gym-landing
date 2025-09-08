1c1f811eeba5ce16d230305426ec30837328c399
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2025-09-08 18:36:03 -0500

    fix(styles): correct Tailwind imports and replace tw-animate-css with tailwindcss-animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,18 @@
 import type { Config } from "tailwindcss"
+import animate from "tailwindcss-animate"
 
 const config: Config = {
     content: [
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/styles/**/*.{css,scss}", // para tus estilos globales
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{css,scss}", // para tus estilos globales
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    animate,
+  ],
 }
 
 export default config
